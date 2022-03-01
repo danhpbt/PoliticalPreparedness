21fb402bea5b3522daeaa1f7a7ca8fa0f0bf82e6
Danh Phan <pbtdanh@yahoo.com>
2022-03-01 01:21:29 -0800

    * run election fragment

--- app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsFragment.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsFragment.kt
@@ -6,10 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.android.politicalpreparedness.R
 import com.example.android.politicalpreparedness.base.BaseFragment
 import com.example.android.politicalpreparedness.databinding.FragmentElectionBinding
+import com.example.android.politicalpreparedness.election.adapter.ElectionListAdapter
+import com.example.android.politicalpreparedness.election.adapter.ElectionListener
+import com.example.android.politicalpreparedness.network.jsonadapter.ElectionAdapter
 import org.koin.android.ext.android.inject
 
 class ElectionsFragment: BaseFragment() {
@@ -33,23 +38,48 @@ class ElectionsFragment: BaseFragment() {
 
         //TODO: Populate recycler adapters
 
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_election, container, false)
+        //binding = DataBindingUtil.inflate(inflater, R.layout.fragment_election, container, false)
+        binding = FragmentElectionBinding.inflate(inflater)
+        binding.lifecycleOwner = this
         binding.viewModel = _viewModel
 
+        var upcomingElectionAdapter = ElectionListAdapter(upcomingElectionListener)
+        var followedElectionAdapter = ElectionListAdapter(followedElectionListener)
 
+        binding.rvElections.adapter = upcomingElectionAdapter
+        binding.rvSavedElections.adapter = followedElectionAdapter
+
+        _viewModel.upcomingElections.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                upcomingElectionAdapter.submitList(it)
+            }
+        })
+        _viewModel.followedElections.observe(viewLifecycleOwner, Observer {
+            it?.let{
+                followedElectionAdapter.submitList(it)
+            }
+        })
 
         return binding.root
 
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        binding.lifecycleOwner = this
+    val upcomingElectionListener = ElectionListener{it ->
+        findNavController().navigate(
+            ElectionsFragmentDirections.actionElectionsFragmentToVoterInfoFragment(
+                it.id,
+                it.division
+            )
+        )
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-
+    val followedElectionListener = ElectionListener{ it ->
+        findNavController().navigate(
+            ElectionsFragmentDirections.actionElectionsFragmentToVoterInfoFragment(
+                it.id,
+                it.division
+            )
+        )
     }
 
     //TODO: Refresh adapters when fragment loads
--- app/src/main/java/com/example/android/politicalpreparedness/network/CivicsApiService.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/network/CivicsApiService.kt
@@ -1,20 +1,25 @@
 package com.example.android.politicalpreparedness.network
 
+import com.example.android.politicalpreparedness.network.jsonadapter.ElectionAdapter
 import com.example.android.politicalpreparedness.network.models.ElectionResponse
 import com.example.android.politicalpreparedness.network.models.RepresentativeResponse
 import com.example.android.politicalpreparedness.network.models.VoterInfoResponse
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.squareup.moshi.Moshi
+import com.squareup.moshi.adapters.Rfc3339DateJsonAdapter
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
+import java.util.*
 
 private const val BASE_URL = "https://www.googleapis.com/civicinfo/v2/"
 
 // TODO: Add adapters for Java Date and custom adapter ElectionAdapter (included in project)
 private val moshi = Moshi.Builder()
+        .add(ElectionAdapter())
+        .add(Date::class.java, Rfc3339DateJsonAdapter().nullSafe())
         .add(KotlinJsonAdapterFactory())
         .build()
 
@@ -32,18 +37,18 @@ private val retrofit = Retrofit.Builder()
 interface CivicsApiService {
     //TODO: Add elections API Call
     @GET("elections")
-    fun getElections(): ElectionResponse
+    suspend fun getElections(): ElectionResponse
 
     //TODO: Add voterinfo API Call
     @GET("voterinfo")
-    fun getVoterInfo(
+    suspend fun getVoterInfo(
         @Query("electionId") electionId: Int,
         @Query("address") address: String
     ): VoterInfoResponse
 
     //TODO: Add representatives API Call
     @GET("representatives")
-    fun getRepresentatives(
+    suspend fun getRepresentatives(
         @Query("address") address: String
     ): RepresentativeResponse
 }
